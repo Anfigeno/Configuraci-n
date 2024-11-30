ffb7b0f87188be3c018c395269cad3dd55f43497
Anfígeno <dolorcriticodevastador@proton.me>
2024-11-30 13:58:05 -0500

    [Fish] No recuerdo

--- fish/config.fish
+++ fish/config.fish
@@ -1,14 +1,15 @@
+source ~/.config/fish/path.fish
 source ~/.config/fish/env.fish
 
 if status is-interactive
-  alias ls="eza --icons always --color always --long --git --no-user --no-time --no-permissions --no-filesize --all  --sort type --grid"
-  alias ll="eza --icons always --color always --long --git --all --sort type"
-  alias tree="eza --icons always --color always --long --git --no-user --no-time --no-permissions --no-filesize --all  --sort type --tree --git-ignore"
-  alias cat="batcat"
-  alias zellij="zellij attach -c Zellij"
+    alias ls="eza --icons always --color always --long --git --no-user --no-time --no-permissions --no-filesize --all  --sort type --grid"
+    alias ll="eza --icons always --color always --long --git --all --sort type"
+    alias tree="eza --icons always --color always --long --git --no-user --no-time --no-permissions --no-filesize --all  --sort type --tree --git-ignore"
+    alias cat="batcat"
+    alias zellij="zellij attach -c Zellij"
 end
 
-set -g fzf_preview_dir_cmd eza --icons always --color always --long --git --no-user --no-time --no-permissions --no-filesize --all  --sort type
+set -g fzf_preview_dir_cmd eza --icons always --color always --long --git --no-user --no-time --no-permissions --no-filesize --all --sort type
 
 # set -g tide_pwd_bg_color cyan
 set -g tide_pwd_color_dirs cyan
